a44324f3c994ae5005ca92e632d60ec40d8fef4e
Safi100 <safinafi12@gmail.com>
2023-02-10 16:01:54 +0200

    add user to todos

--- models/todo.js
+++ models/todo.js
@@ -15,6 +15,10 @@ const userSchema = new Schema({
     createdAt:{
         type: Date,
         default: Date.now
+    },
+    author : {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
     }
 })
 
