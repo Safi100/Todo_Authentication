4b9549b879be7f54f8cafc13fce23e5194c0d6d4
Safi100 <safinafi12@gmail.com>
2023-02-13 00:24:55 +0200

    create isAuthor middleware

--- middleware.js
+++ middleware.js
@@ -1,3 +1,5 @@
+const Todo = require("./models/todo")
+
 module.exports.isAuthenticated = (req, res, next) => {
     if(!req.session.user){
         return res.redirect('/login')
@@ -6,6 +8,19 @@ module.exports.isAuthenticated = (req, res, next) => {
 }
 module.exports.LoggedIn = (req, res, next) => {
     if(req.session.user){
+        return res.redirect('/')
+    }
+    next()
+}
+module.exports.isAuthor = async (req, res, next) => {
+    const id = req.params.id
+    const todo = await Todo.findById(id)
+    if(!todo){
+        req.flash('error', 'Cannot find that todo!')
+        return res.redirect('/')
+    }
+    if(!todo.author.equals(req.session.user)){
+        req.flash('error', "You don't have permission to do that!")
         return res.redirect('/')
     }
     next()
